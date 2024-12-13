7a3af7c01bbd3ddd3a050b4336752c3b25395a30
VARUN KUMAR <varun91thakur@gmail.com>
2024-12-13 23:12:46 +0530

    [varun]Added : palindrome

---
+++ RsPec/palindrome/palindrome.rb
@@ -0,0 +1,13 @@
+class Palindrome
+  def palindrome_number?(str)
+    left,right=0,str.length-1
+    while left < right
+      if str[left] != str [right]
+        return false
+      end
+      left+=1
+      right-=1
+    end
+      return true
+  end
+  end
